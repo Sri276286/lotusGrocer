d0c8c1da5f85b8c0ce9574ea8c42db98301ae70a
Sri276286 <sri276286@gmail.com>
2020-09-26 18:52:22 +0530

    Update categories with tippy.js

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -1,7 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../services/categories.service';
-import { LotusCommonService } from '../services/common.service';
-import { SubCategListPage } from './subcateg-list/subcateg-list.page';
+import tippy from 'tippy.js';
 
 @Component({
     selector: 'lotus-categories',
@@ -10,8 +9,9 @@ import { SubCategListPage } from './subcateg-list/subcateg-list.page';
 })
 export class CategoryPage implements OnInit {
     categories;
+    tippyInstance;
+    subcategory;
     constructor(private categoryService: CategoryService) {
-
     }
 
     ngOnInit() {
@@ -19,5 +19,25 @@ export class CategoryPage implements OnInit {
             console.log('res ', res);
             this.categories = res && res.categories;
         });
+    }
+
+    showSub(category) {
+        console.log('aaaa ', category.sub);
+        this.subcategory = category.sub;
+        const template = document.getElementById('template');
+        tippy('#categ', {
+            content: template.innerHTML,
+            allowHTML: true,
+            interactive: true,
+            placement: "bottom",
+            appendTo: document.body,
+            theme: 'light'
+        });
+        // this.tippyInstance = template.innerHTML._tippy;
+    }
+
+    removeSub() {
+        // this.tippyInstance.destroy();
+        this.subcategory = [];
     }
 }
